75794d7ce7e11d230c263b3f50c701a19f03f96f
avazbek04 <avazbekismoilov0404@gmail.com>
2024-12-13 21:08:43 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def dec_to_bin(n):
+    return bin(n)[2:]  # bin() funksiyasi bilan 10-likdan 2-likka o‘tkazish
+
+# Misol
+decimal_number = 10
+binary_number = dec_to_bin(decimal_number)
+print(f"Decimal: {decimal_number} -> Binary: {binary_number}")
