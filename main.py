c1a8c53e5b03c9b1898414b771d45c61d6aaca79
avazbek04 <avazbekismoilov0404@gmail.com>
2024-12-13 21:29:46 +0300

    oxirgi

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-def dec_to_hex(n):
-    return hex(n)[2:].upper()  # hex() funksiyasi bilan 10-likdan 16-likka o‘tkazish
-
-# Misol
-decimal_number = 10
-hexadecimal_number = dec_to_hex(decimal_number)
-print(f"Decimal: {decimal_number} -> Hexadecimal: {hexadecimal_number}")
+# nima gaplar avaz
