22021aeac444707529aa82dea0ea4df1ddd1f693
avazbek04 <avazbekismoilov0404@gmail.com>
2024-12-13 21:24:41 +0300

    10 lik dan 8 likga

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def dec_to_bin(n):
-    return bin(n)[2:]  # bin() funksiyasi bilan ''10-likdan 2-likka o‘tkazish''
+def dec_to_oct(n):
+    return oct(n)[2:]  # oct() funksiyasi bilan 10-likdan 8-likka o‘tkazish
 
 # Misol
 decimal_number = 10
-binary_number = dec_to_bin(decimal_number)
-print(f"Decimal: {decimal_number} -> Binary: {binary_number}")
+octal_number = dec_to_oct(decimal_number)
+print(f"Decimal: {decimal_number} -> Octal: {octal_number}")
